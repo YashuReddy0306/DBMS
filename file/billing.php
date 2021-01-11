3d7013740d5cbd26dd004d3a08b0a737ba0ee39c
blueplanet18 <69891792+blueplanet18@users.noreply.github.com>
2021-01-10 22:14:02 -0800

    Add files via upload

--- file/billing.php
+++ file/billing.php
@@ -1,11 +1,23 @@
+<?php 
+error_reporting(E_ALL ^ E_WARNING);
+$db=mysqli_connect('localhost','root','','onlinecourier') or die("could not connect to database");
 
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Billing</title>
+	 <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js">
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+    
+</head>
 </head>
 <body>
-	<h2></h2>
 
 </body>
 </html>
--- file/ship.php
+++ file/ship.php
@@ -1,33 +1,52 @@
 <?php
 error_reporting(E_ALL ^ E_WARNING);
-$errors3=array();
+$errors=array();
+$min=50;
 
 $db=mysqli_connect('localhost','root','','onlinecourier') or die("could not connect to database");
+if(isset($_POST['submit'])){
 
 $UID=$_GET['a'];
-$name1=mysqli_real_escape_string($db,$_POST['name1']);
-$address1=mysqli_real_escape_string($db,$_POST['address1']);
-$phone_11=mysqli_real_escape_string($db,$_POST['phone_11']);
-$phone_21=mysqli_real_escape_string($db,$_POST['phone_21']);
-$name2=mysqli_real_escape_string($db,$_POST['name2']);
-$address2=mysqli_real_escape_string($db,$_POST['address2']);
-$phone_12=mysqli_real_escape_string($db,$_POST['phone_12']);
-$phone_22=mysqli_real_escape_string($db,$_POST['phone_22']);
+$name1=$_POST['name1'];
+$address1=$_POST['address1'];
+$phone1=$_POST['phone_11'];
+$name2=$_POST['name2'];
+$address2=$_POST['address2'];
+$phone2=$_POST['phone_12'];
+$weight=$_POST['weight'];
+$price=$weight*$min+$min;
+$date=date("m/d/Y");
+$tid=uniqid();
 
-if(empty($name1)) { array_push($errors3,"sname required"); }
-if(empty($name2)) { array_push($errors3,"rname required"); }
-if(empty($address1)) { array_push($errors3,"saddress required"); }
-if(empty($address2)) { array_push($errors3,"raddress required"); }
-if(empty($phone_11)) { array_push($errors3,"sphone required"); }
-if(empty($phone_12)) { array_push($errors3,"rphone required"); }
 
-if(count($errors3)==0){
-	$query_run1="insert into shipping(userID,sname,saddress,sphone1,sphone2,rname,raddress,rphone1,rphone2) values('$UID','$name1','$address1',$phone_11,$phone_21,'$name2','$address2',$phone_21,$phone_22)";
+if(empty($name1)) { array_push($errors,"sname required"); }
+if(empty($name2)) { array_push($errors,"rname required"); }
+if(empty($address1)) { array_push($errors,"saddress required"); }
+if(empty($address2)) { array_push($errors,"raddress required"); }
+if(empty($phone_11)) { array_push($errors,"sphone required"); }
+if(empty($phone_12)) { array_push($errors,"rphone required"); }
 
-	mysqli_query($db,$query_run1);
+
+if(count($errors)==0)
+{
+	$query="insert into ship(userID,trackID,sname,saddress,sphone,rname,raddress,rphone,date,weight,price) values($UID,$tid,$name1,$address,$phone1,$name2,$address2,$phone2,$date,$weight,$price)";
+
+	if(!mysqli_query($db,$query)){
+		?><script >
+			alert("data not inserted");
+		</script>
+		<?php
+	}else{
 	
-	header("location:billing.php");
+	header("location:billing.php");}
+}else{
+	?><script >
+		alert("error.");
+	</script>
+	<?php
 }
+}
+
 
 ?>
 
@@ -38,27 +57,12 @@ if(count($errors3)==0){
 	<link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
     <script
   src="https://code.jquery.com/jquery-3.5.1.min.js">
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
-    <script type="text/javascript">
-    	$(document).ready(function(){
 
-    		var html= '<tr><td><input class="form-control" type="text" name="desc" required=""></td><td><input class="form-control"  type="text" name="weight" required=""></td><td><input class="form-control"  type="text" name="Instruction"></td><td><input class="btn btn-warning"  type="button" name="remove" id="remove" value="Remove"></td></tr>';
-
-    		var x=1;
-
-    		$("#add").click(function(){
-    			$("#table_field").append(html);
-    		});
-
-    		$("#table_field").on('click','#remove',function(){
-    			$(this).closest('tr').remove();
-    		});
-
-    	});
-    </script>
 </head>
 <style type="text/css">
 	body{
@@ -165,7 +169,7 @@ if(count($errors3)==0){
 	*{
 		box-sizing: border-box;
 	}
-	input[type=text],input[type=email],input[type=tel],input[type=password] {
+	input[type=text],input[type=number],input[type=tel] {
 		width: 100%;
 		padding: 0.55em;
 		border: 1px solid #ccc;
@@ -193,17 +197,14 @@ if(count($errors3)==0){
 			<div class="col-25"><label for="name">Name </label><span style="color: red;">*</span></div>
 			<div class="col-75"><input type="text" name="name1" required=""></div></div>
 			<div class="row">
-				<div class="col-25"><label for="address">Address</label><span style="color: red;">*</span></div>
-				<div class="col-75"><input type="text" name="address1" required=""></div>
+				<div class="col-25"><label for="from">Address</label><span style="color: red;">*</span></div>
+				<div class="col-75"> <input class="form-control" id="from" type="text" name="address1" placeholder="Enter a location"> </div>
 			</div>
 			<div class="row">
 				<div class="col-25"><label for="phone">Phone</label><span style="color: red;">*</span></div>
 				<div class="col-75"><input type="tel" name="phone_11" required="" pattern="[0-9]{10}"></div>
 			</div>
-			<div class="row">
-				<div class="col-25"><label for="phone">Alternate Phone</label></div>
-				<div class="col-75"><input type="tel" name="phone_21" pattern="[0-9]{10}"></div>
-			</div>
+			
 		<hr style="border: 1px solid black;">
 		
 	</div>
@@ -214,44 +215,48 @@ if(count($errors3)==0){
 			<div class="col-25"><label for="name">Name</label><span style="color: red;">*</span></div>
 			<div class="col-75"><input type="text" name="name2" required=""></div></div>
 			<div class="row">
-				<div class="col-25"><label for="address">Address</label><span style="color: red;">*</span></div>
-				<div class="col-75"><input type="text" name="address2" required=""></div>
+				<div class="col-25"><label for="to">Address</label><span style="color: red;">*</span></div>
+				<div class="col-75" ><input class="form-control" id="to" type="text" name="address2" placeholder="Enter a location"> </div>
 			</div>
 			<div class="row">
 				<div class="col-25"><label for="phone_1">Phone</label><span style="color: red;">*</span></div>
 				<div class="col-75"><input type="tel" name="phone_12" required="" pattern="[0-9]{10}"></div>
 			</div>
-			<div class="row">
-				<div class="col-25"><label for="phone">Alternate Phone</label></div>
-				<div class="col-75"><input type="tel" name="phone_22" pattern="[0-9]{10}"></div>
-			</div>
+			
 		<hr style="border: 1px solid black;">
 
 		
 	</div>
-	<div class="Parcel">
+</div>
+</form>
+
+	<div class="Parcel" style="margin-top: 10px;">
 		<h2 style="font-weight: bold; font-size: 1.5em; font-family: 'times new roman',serif; text-align: center; font-variant: small-caps;text-decoration: underline overline ; color: red;">Parcel Details:</h2>
-			<div class="input_field">
-				<table class="table table-bordered" id="table_field">
+		<form method="post" action="billing.php" name="par">
+			<div class="input_field" >
+				<table class="table table-bordered" id="table_field" >
+					
+
 					<tr>
-						<th>Parcel Description <span style="color: red;">*</span></th>
-						<th>Parcel Weight <span style="color: red;">*</span></th>
-						<th>Special Instruction</th>
-						<th>Add/Remove</th>
+						<th>Item quanity <span style="color: red;">*</span></th>
+						<th>Total Weight(kg) <span style="color: red;">*</span></th>
+						<th>Min Price</th>
 					</tr>
-					<tr>
-						<td><input class="form-control" type="text" name="desc" required=""></td>
-						<td><input class="form-control"  type="text" name="weight" required=""></td>
-						<td><input class="form-control"  type="text" name="Instruction"></td>
-						<td><input class="btn btn-warning"  type="button" name="add" id="add" value="Add"></td>
+				
+					<tr name="item">
+						<td><input class="form-control" type="number" name="no" required="" ></td>
+						<td><input class="form-control"  type="number" name="weight" id="wei" required="" ></td>
+						<td><input class="form-control" type="text" name="min" value="<?php echo $min; ?>" readonly="" ></td>
 					</tr>
 					
 				</table>
 			</div>
-		</div>
-			<button class="btn btn-danger" style="font-size: 1.1em;margin-right: 45%; margin-bottom: 5em;"><a href="billing.php?a=<?php echo $UID;?>">Billing</a></button>
+		
+
+			<button class="btn btn-danger" name="submit " style="font-size: 0.8em;margin-right: 45%; margin-bottom: 5em;" >Confirm Booking</button>
 		</form>	
 	</div>
+	
 
 </body>
 </html>
